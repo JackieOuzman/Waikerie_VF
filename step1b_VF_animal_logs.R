67e9bfebe097611c803e69f93f8c800daa3c9755
Ouzman <ouz001@csiro.au>
2022-11-24 14:37:30 +1030

    step 1b done trim time

--- step1b_VF_animal_logs.R
+++ step1b_VF_animal_logs.R
@@ -30,7 +30,7 @@ GPS$local_time <- as.POSIXct(GPS$local_time,  tz = "Australia/Melbourne")
 str(Behavioural)
 Behavioural$date_time <- as.POSIXct(Behavioural$date_time,  tz = "Australia/Melbourne")
 str(HOBO) ## I have no idea what if we can use the date / time clm
-HOBO$date_time <- as.POSIXct(HOBO$date_time,  tz = "Australia/Melbourne")
+HOBO$`Date Time` <- as.POSIXct(HOBO$`Date Time`,  tz = "Australia/Melbourne")
 
 ############################################################################################
 ############                  Turn into spatial data          ##############################
@@ -70,6 +70,7 @@ ggplot() +
   geom_sf(data = VF, color = "black", fill = NA) +
   geom_sf(data = GPS_sf_trans ,alpha = 0.03) +
   theme_bw()+
+  facet_wrap(.~ date)+
   theme(legend.position = "none",
         axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
   labs(title = "")
@@ -77,14 +78,6 @@ ggplot() +
 
 ################################################################################
 #### filtering out GPS data based on times start and end of the trial
-
-GPS_sf_trans <- GPS_sf_trans %>% 
-  filter(
-    local_time >=  ymd_hms("2018-03-13 09:30:00", tz= "Australia/Melbourne"))
-
-GPS_sf_trans <- GPS_sf_trans %>% 
-  filter(
-    local_time <=  ymd_hms("2018-03-16 15:30:00", tz= "Australia/Melbourne"))
 
 
 # Times sheep were brought in each day from what I can understand from the write up HOBO data;
@@ -106,11 +99,7 @@ day_16 <-day_16 %>%  filter(treatment == "33_percent"| treatment == "66_percent"
 
 
 str(day_13)
-#-----Up to here----- The below code is not working#
 
-#Next steps boundaries trim location and time
-## merge in other animal data
-## look for weather data ? anything else??
 
 # keep everything after before yarding and after yarding
 
@@ -118,46 +107,35 @@ min(day_13$local_time)
 max(day_13$local_time)
 
 
-day_13_clean <- day_13%>% filter( between(local_time, ymd_hms("2018-03-13 09:30:00"), ymd_hms("2022-06-29 15:30:00")) )
-day_14_clean <- day_14%>% filter( between(local_time, ymd_hms("2018-03-13 09:30:00"), ymd_hms("2022-06-29 15:30:00")) )
-day_15_clean <- day_15%>% filter( between(local_time, ymd_hms("2018-03-13 09:30:00"), ymd_hms("2022-06-29 15:30:00")) )
-day_16_clean <- day_16%>% filter( between(local_time, ymd_hms("2018-03-13 09:30:00"), ymd_hms("2022-06-29 15:30:00")) )
+day_13_clean <- day_13%>% filter( between(local_time, ymd_hms("2018-03-13 09:30:00"), ymd_hms("2018-03-13 15:30:00")) )
+day_14_clean <- day_14%>% filter( between(local_time, ymd_hms("2018-03-14 09:30:00"), ymd_hms("2018-03-14 15:30:00")) )
+day_15_clean <- day_15%>% filter( between(local_time, ymd_hms("2018-03-15 09:30:00"), ymd_hms("2018-03-15 15:30:00")) )
+day_16_clean <- day_16%>% filter( between(local_time, ymd_hms("2018-03-16 09:30:00"), ymd_hms("2018-03-16 15:30:00")) )
 
 
 
 
 
-### put it back togther 
+### put it back together 
 
 GPS_trim_time <- rbind(day_13_clean, day_14_clean, day_15_clean, day_16_clean)
 
-rm(day_28, day_29_clean, day_30_clean, day_1_clean, day_2)
+rm(day_13_clean, day_14_clean, day_15_clean, day_16_clean, day_13, day_14, day_15, day_16)
 
 ########################################################################################
 
 
-
-
-
-### remove the water and other animals logs
-
-
-animals_GPS_trim_time <- animals_GPS_trim_time %>% 
-  filter(Sheep_ID !=  "other") %>% 
-  filter(Sheep_ID !=  "water_pt")
-
+## check
 
 ggplot() +
-  geom_sf(data = Chiswick_hard_fence_bound, color = "black", fill = NA) +
-  geom_sf(data = VF_paddock, color = "black", fill = NA) +
-  geom_sf(data = Chiswick_hard_fence_bound_buff, color = "black", fill = NA) +
-  geom_sf(data = water_pt ,color ="Blue") +
-  geom_sf(data = animal_GPS_data_sf_trans ,alpha = 0.05) +
+  geom_sf(data = hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = VF, color = "black", fill = NA) +
+  geom_sf(data = GPS_trim_time ,alpha = 0.03) +
   facet_wrap(.~ date)+
   theme_bw()+
   theme(legend.position = "none",
         axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
-  labs(title = "All animal logs between 28/06 at 09:50 and 02/07 at 10:10",
+  labs(title = "All animal logs trimmed time",
        subtitle = "log when animals were yarded removed")
 
 
@@ -168,7 +146,9 @@ ggplot() +
 #I think this should be the end of step 1.
 
 
-
+#Next steps boundaries trim location and time
+## merge in other animal data
+## look for weather data ? anything else??
 
 
 
@@ -177,7 +157,7 @@ ggplot() +
 
 
 
-output_path <- "W:/VF/Sheep_Chiswick_2022/animal_logs/jax_working"  #animals_GPS_trim_time
+output_path <- "W:/VF/Optimising_VF/Waikerie/data_prep/"  #animals_GPS_trim_time
 
 
 ############################################################################################################################
@@ -186,24 +166,24 @@ output_path <- "W:/VF/Sheep_Chiswick_2022/animal_logs/jax_working"  #animals_GPS
 ## convert the geom clm into x and y clms
 
 
-coordinates <-as.data.frame( st_coordinates(animals_GPS_trim_time))
-animals_GPS_trim_time_df <- as.data.frame(animals_GPS_trim_time)
+coordinates <-as.data.frame( st_coordinates(GPS_trim_time))
+GPS_trim_time_df <- as.data.frame(GPS_trim_time)
 
-animals_GPS_trim_time_df <- animals_GPS_trim_time_df %>% 
+GPS_trim_time_df <- GPS_trim_time_df %>% 
   dplyr::select(-"geometry")
 
 
-animals_GPS_trim_time <-   cbind(animals_GPS_trim_time_df,coordinates )
+GPS_trim_time <-   cbind(GPS_trim_time_df,coordinates )
 ## ensure the date and time clms are outputting and outputting in the correct format.
 
 
-animals_GPS_trim_time$local_time <-   format(animals_GPS_trim_time$local_time, usetz=TRUE)
-animals_GPS_trim_time$GMT        <-   format(animals_GPS_trim_time$GMT, usetz=TRUE)
-animals_GPS_trim_time$start_fence <-  format(animals_GPS_trim_time$start_fence, usetz=TRUE)
-animals_GPS_trim_time$end_fence    <- format(animals_GPS_trim_time$end_fence, usetz=TRUE)
-animals_GPS_trim_time$start_trial    <- format(animals_GPS_trim_time$start_trial, usetz=TRUE)
+GPS_trim_time$local_time <-   format(GPS_trim_time$local_time, usetz=TRUE)
+GPS_trim_time$GMT        <-   format(GPS_trim_time$GMT, usetz=TRUE)
+GPS_trim_time$start_fence <-  format(GPS_trim_time$start_fence, usetz=TRUE)
+GPS_trim_time$end_fence    <- format(GPS_trim_time$end_fence, usetz=TRUE)
+GPS_trim_time$start_trial    <- format(GPS_trim_time$start_trial, usetz=TRUE)
 
-write.csv(animals_GPS_trim_time, 
+write.csv(GPS_trim_time, 
           paste0(output_path,"/animals_GPS_trim_time_step1.csv"), 
           row.names=FALSE)
 #############################################################
