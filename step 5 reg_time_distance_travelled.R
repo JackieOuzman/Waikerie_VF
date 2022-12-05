856033519a29a016f99b207e6e8b469bae18f1f0
Ouzman <ouz001@csiro.au>
2022-12-05 14:53:48 +1030

    all of the data

--- step 5 reg_time_distance_travelled.R
+++ step 5 reg_time_distance_travelled.R
@@ -1,4 +1,4 @@
-### template for creating some variable
+### reg time step and distnace travelled
 
 ################################################################################
 library(dplyr)
@@ -54,16 +54,6 @@ regular_time_interval <-data.frame(time_step = seq(from = ymd_hms(start),
                                                    to = ymd_hms(end), 
                                                    by = '10 mins'))
 
-GPS_Dist %>% distinct(sheep)  ### 26 sheep ID I need regular time interval for each sheep
-
-
-#regular_time_interval_per_sheep_ID ? this may need to be a loop or function
-
-regular_time_interval_2 <- regular_time_interval %>% 
-  dplyr::mutate(Time_sheep = paste0(time_step,"_", 2))
-regular_time_interval_3 <- regular_time_interval %>% 
-  dplyr::mutate(Time_sheep = paste0(time_step,"_", 3))
-
 ################################################################################
 #### ----   Need to round the local time to the closest 10 min  -------------- ####
 ################################################################################
@@ -74,92 +64,146 @@ GPS_Dist <- GPS_Dist %>%
                 Time_sheep = paste0(round_local_time,"_", sheep) )
 
 
+
+rm(end,start, time.duration, time.interval)
+
+sheep_list <- GPS_Dist %>% distinct(sheep) %>%  arrange(sheep)
+### 26 sheep ID I need regular time interval for each sheep
+### List of sites I want to run analysis for:
+
+sheep_list <- c(1:36)
+#sheep_list <- c(1:2)
+
+### as a function
+for (sheep_list in sheep_list){
+  
+################################################################################  
+#regular_time_interval_per_sheep_ID
+################################################################################
+  regular_time_interval_sheep <- regular_time_interval %>% 
+    dplyr::mutate(Time_sheep = paste0(time_step,"_", sheep_list))
+  
 ################################################################################
 #### --------------    Join regular time step to dataset  -------------- ####
-################################################################################
-#For all joins, rows will be duplicated if one or more rows in x matches multiple rows in y.
-# Need to deal with duplicates before joining the data.
+################################################################################  
+  
+  GPS_sheep <- GPS_Dist %>%  filter(sheep == sheep_list)
+  GPS_sheep <- GPS_sheep %>% 
+    dplyr::distinct(Time_sheep, .keep_all = TRUE)              
+  
+  GPS_sheep_reg_time <- left_join(regular_time_interval_sheep, GPS_sheep)
+
+  #### Trim the regular time step to match the end of sheep time
+
+  start_sheep <- min(GPS_sheep$local_time, na.rm = TRUE)  
+  end_sheep <-   max(GPS_sheep$local_time, na.rm = TRUE) 
+  start_sheep <- round_date(start_sheep, unit="10 mins")
+  end_sheep <- round_date(end_sheep, unit="10 mins") 
+  
+  ## trim the joined data to the sheeps ID time in the trial 
+  
+  
+  GPS_sheep_reg_time <- GPS_sheep_reg_time %>% 
+    dplyr::filter(between(local_time, ymd_hms(start_sheep), ymd_hms(end_sheep)))  
+  
+  ################################################################################
+  #### Do some cals  steps or distance travelled since last logged point ---- ####
+  ################################################################################ 
+
+    GPS_sheep_reg_time <- GPS_sheep_reg_time %>% 
+    arrange(local_time)
+  
+  GPS_sheep_reg_time <- GPS_sheep_reg_time %>% 
+    dplyr::mutate(step = sqrt( ((lead(X) - X)^ 2) + ((lead(Y) - Y)^ 2) ) )
+  
+  rm(
+    GPS_sheep,
+    regular_time_interval_sheep
+    )
+  name <- paste0("GPS_sheep_reg_time_step", sheep_list)
+  assign(name,GPS_sheep_reg_time)
+  
+  }       
+
+file_list <- data.frame(name_df = paste0("GPS_sheep_reg_time_step",c(1:36)))
+                
+GPS_sheep_reg_time_step_all <- rbind(
+  GPS_sheep_reg_time_step1,
+  GPS_sheep_reg_time_step2,
+  GPS_sheep_reg_time_step3,
+  GPS_sheep_reg_time_step4,
+  GPS_sheep_reg_time_step5,
+  GPS_sheep_reg_time_step6,
+  GPS_sheep_reg_time_step7,
+  GPS_sheep_reg_time_step8,
+  GPS_sheep_reg_time_step9,
+  GPS_sheep_reg_time_step10,
+  GPS_sheep_reg_time_step11,
+  GPS_sheep_reg_time_step12,
+  GPS_sheep_reg_time_step13,
+  GPS_sheep_reg_time_step14,
+  GPS_sheep_reg_time_step15,
+  GPS_sheep_reg_time_step16,
+  GPS_sheep_reg_time_step17,
+  GPS_sheep_reg_time_step18,
+  GPS_sheep_reg_time_step19,
+  GPS_sheep_reg_time_step20,
+  GPS_sheep_reg_time_step21,
+  GPS_sheep_reg_time_step22,
+  GPS_sheep_reg_time_step23,
+  GPS_sheep_reg_time_step24,
+  GPS_sheep_reg_time_step25,
+  GPS_sheep_reg_time_step26,
+  GPS_sheep_reg_time_step27,
+  GPS_sheep_reg_time_step28,
+  GPS_sheep_reg_time_step29,
+  GPS_sheep_reg_time_step30,
+  GPS_sheep_reg_time_step31,
+  GPS_sheep_reg_time_step32,
+  GPS_sheep_reg_time_step33,
+  GPS_sheep_reg_time_step34,
+  GPS_sheep_reg_time_step35,
+  GPS_sheep_reg_time_step36)
 
 
-##----- sheep 2 -----###
-GPS_sheep2 <- GPS_Dist %>%  filter(sheep == 2)
-GPS_sheep2 <- GPS_sheep2 %>% 
-  dplyr::distinct(Time_sheep, .keep_all = TRUE) # this is where you could do other options group_by average, max ??
-
-GPS_sheep2_reg_time <- left_join(regular_time_interval_2, GPS_sheep2)
 
 
-##----- sheep 3 -----###
-GPS_sheep3 <- GPS_Dist %>%  filter(sheep == 3)
-GPS_sheep3 <- GPS_sheep3 %>% 
-  dplyr::distinct(Time_sheep, .keep_all = TRUE) # this is where you could do other options group_by average, max ??
-
-GPS_sheep3_reg_time <- left_join(regular_time_interval_3, GPS_sheep3)
-
-
-
-
-#--- comment here if I have multiple rows for the same 10 mins this method only hangs onto the first row that matches (due to removing duplictae step).
-#--- This is not ideal but is similar to what the newer collars do.
-#--- an option could be to have a smaller time step and then do the calculations that are needed. 
-#--- Say distance from the fence or number of steps.
-#--- then you can use a groupby function using '10min time step clm' or 'round to 10min clm' and report on the max value for the variable.
-
-#--- other comments is the missing rows of data will need to be filled in but how?
-
-
-## why do I have so much un - joined data - the sheep 2 did not run for the length of the trial.
-
-## so for this example I just need to trim the df to match the end of sheep 2
-
-### -------------------------------------------------------------####
-
-start_sheep2 <- min(GPS_sheep2$local_time, na.rm = TRUE)  
-end_sheep2 <-   max(GPS_sheep2$local_time, na.rm = TRUE) 
-start_sheep2 <- round_date(start_sheep2, unit="10 mins") #"2018-03-13 09:50:00 ACDT"
-end_sheep2 <- round_date(end_sheep2, unit="10 mins") #"2018-03-14 15:30:00 ACDT"
-
-## trim the joined data to the sheeps ID time in the trial 
-
-GPS_sheep2_reg_time <- GPS_sheep2_reg_time %>% 
-  dplyr::filter(between(local_time, ymd_hms(start_sheep2), ymd_hms(end_sheep2)))
-
-### -------------------------------------------------------------####
-
-start_sheep3 <- min(GPS_sheep3$local_time, na.rm = TRUE)  
-end_sheep3 <-   max(GPS_sheep3$local_time, na.rm = TRUE) 
-start_sheep3 <- round_date(start_sheep3, unit="10 mins") #"2018-03-13 10:20:00 ACDT"
-end_sheep3 <- round_date(end_sheep3, unit="10 mins") #"2018-03-14 15:30:00 ACDT"
-
-## trim the joined data to the sheeps ID time in the trial 
-
-GPS_sheep3_reg_time <- GPS_sheep3_reg_time %>% 
-  dplyr::filter(between(local_time, ymd_hms(start_sheep3), ymd_hms(end_sheep3)))
-
-
-
-
-
-
-################################################################################
-#### Do some cals  steps or distance travelled since last logged point ---- ####
-################################################################################
-GPS_sheep2_reg_time <- GPS_sheep2_reg_time %>% 
-  arrange(local_time)
-
-GPS_sheep2_reg_time <- GPS_sheep2_reg_time %>% 
-  dplyr::mutate(step = sqrt( ((lead(X) - X)^ 2) + ((lead(Y) - Y)^ 2) ) )
-
-### -------------------------------------------------------------####
-GPS_sheep3_reg_time <- GPS_sheep3_reg_time %>% 
-  arrange(local_time)
-
-GPS_sheep3_reg_time <- GPS_sheep3_reg_time %>% 
-  dplyr::mutate(step = sqrt( ((lead(X) - X)^ 2) + ((lead(Y) - Y)^ 2) ) )
-
-
-str(GPS_sheep2_reg_time)
-str(GPS_sheep3_reg_time)
+rm(GPS_sheep_reg_time_step1,
+   GPS_sheep_reg_time_step2,
+   GPS_sheep_reg_time_step3,
+   GPS_sheep_reg_time_step4,
+   GPS_sheep_reg_time_step5,
+   GPS_sheep_reg_time_step6,
+   GPS_sheep_reg_time_step7,
+   GPS_sheep_reg_time_step8,
+   GPS_sheep_reg_time_step9,
+   GPS_sheep_reg_time_step10,
+   GPS_sheep_reg_time_step11,
+   GPS_sheep_reg_time_step12,
+   GPS_sheep_reg_time_step13,
+   GPS_sheep_reg_time_step14,
+   GPS_sheep_reg_time_step15,
+   GPS_sheep_reg_time_step16,
+   GPS_sheep_reg_time_step17,
+   GPS_sheep_reg_time_step18,
+   GPS_sheep_reg_time_step19,
+   GPS_sheep_reg_time_step20,
+   GPS_sheep_reg_time_step21,
+   GPS_sheep_reg_time_step22,
+   GPS_sheep_reg_time_step23,
+   GPS_sheep_reg_time_step24,
+   GPS_sheep_reg_time_step25,
+   GPS_sheep_reg_time_step26,
+   GPS_sheep_reg_time_step27,
+   GPS_sheep_reg_time_step28,
+   GPS_sheep_reg_time_step29,
+   GPS_sheep_reg_time_step30,
+   GPS_sheep_reg_time_step31,
+   GPS_sheep_reg_time_step32,
+   GPS_sheep_reg_time_step33,
+   GPS_sheep_reg_time_step34,
+   GPS_sheep_reg_time_step35,
+   GPS_sheep_reg_time_step36)
 
 ##### Think about how to deal with yarding times - I think you may need to remove first step cal after yarding time.
 ## I dont think I need to do anything here - the animals we yarded over night and the data is trimmed to this alreday.
@@ -171,12 +215,12 @@ str(GPS_sheep3_reg_time)
 # day_16 = between(local_time, ymd_hms("2018-03-16 09:30:00"), ymd_hms("2018-03-16 15:30:00")) )
 
 
-rm(GPS_Dist, GPS_sheep2, GPS_sheep3, regular_time_interval, regular_time_interval_2, regular_time_interval_3)
 
-GPS_sheep2_3 <- rbind(GPS_sheep2_reg_time, GPS_sheep3_reg_time)
+
+
 output_path <- "W:/VF/Optimising_VF/Waikerie/data_prep/"  #animals_GPS_trim_time
 
 
-write.csv(GPS_sheep2_3, 
-          paste0(output_path,"/GPS_sheep2_3_reg_time_step_dist.csv"), 
+write.csv(GPS_sheep_reg_time_step_all, 
+          paste0(output_path,"/GPS_sheep_all_reg_time_step_dist.csv"), 
           row.names=FALSE)
