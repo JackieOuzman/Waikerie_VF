ff20b188d4ed83a203439016e1d1bf1bc8c5bd91
Ouzman <ouz001@csiro.au>
2022-12-05 11:36:22 +1030

    changed code to include all the logged points

--- distiance_of_pts_to_line.R
+++ distiance_of_pts_to_line.R
@@ -21,19 +21,20 @@ GPS <-   st_as_sf(GPS,
 
 GPS <- GPS %>% dplyr::select (ID, sheep, treatment,DOT,herd_postion,local_time, date,DOY, geometry)
 names(GPS)
-GPS_sheep2 <- GPS %>%  filter(sheep == 2)
+#GPS_sheep2 <- GPS %>%  filter(sheep == 2)
+GPS_sheep_all <- GPS 
 
-coordinates <-as.data.frame( st_coordinates(GPS_sheep2))
-GPS_sheep2_df <- as.data.frame(GPS_sheep2)
+coordinates <-as.data.frame( st_coordinates(GPS_sheep_all))
+GPS_sheep_all_df <- as.data.frame(GPS_sheep_all)
 
-GPS_sheep2_df <- GPS_sheep2_df %>% 
+GPS_sheep_all_df <- GPS_sheep_all_df %>% 
   dplyr::select(-"geometry")
 
 
-GPS_sheep2_df <-   cbind(GPS_sheep2_df,coordinates )
+GPS_sheep_all_df <-   cbind(GPS_sheep_all_df,coordinates )
 
-# write.csv(GPS_sheep2_df, 
-#           paste0("W:/VF/Optimising_VF/Waikerie/data_prep/","/GPS_sheep2_df_test_dist_func.csv"), 
+# write.csv(GPS_sheep_all_df,
+#           paste0("W:/VF/Optimising_VF/Waikerie/data_prep/","/GPS_sheepall_df_test_dist_func.csv"),
 #           row.names=FALSE)
 ############################################################################################
 ############                  bring in boundaries             ##############################
@@ -61,7 +62,7 @@ ggplot() +
   geom_sf(data = VF_line, color = "red", fill = NA) +
   geom_sf(data = exclusion_zone, color = "blue", fill = NA) +
   
-  geom_sf(data = GPS_sheep2 ,alpha = 0.03) +
+  geom_sf(data = GPS ,alpha = 0.03) +
   theme_bw()+
   theme(legend.position = "none",
         axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
@@ -69,42 +70,42 @@ ggplot() +
 
 ############################################################################################
 
-GPS_sheep2
+GPS
 VF_line
 
-GPS_sheep2 <- GPS_sheep2 %>% 
-  dplyr::mutate(dist_to_VF = st_distance(GPS_sheep2, VF_line))
+GPS <- GPS %>% 
+  dplyr::mutate(dist_to_VF = st_distance(GPS, VF_line))
 ############################################################################################
 ### report if the point is in the exclusion zone
 
 VF <- VF %>%  dplyr::select(Id, geometry)
 
-VF_points <-  st_intersection(GPS_sheep2, VF) %>% 
+VF_points <-  st_intersection(GPS, VF) %>% 
   dplyr::mutate(VF_EX = "inside_VF")
 
-Exclusion_points <-  st_intersection(GPS_sheep2, exclusion_zone)%>% 
+Exclusion_points <-  st_intersection(GPS, exclusion_zone)%>% 
   dplyr::mutate(VF_EX = "outside_VF")
 
 names(VF_points)
 names(Exclusion_points)
 
-GPS_sheep2 <- rbind(VF_points, Exclusion_points)
+GPS_all <- rbind(VF_points, Exclusion_points)
 
-str(GPS_sheep2)
-
-
-GPS_sheep2 <- GPS_sheep2 %>% dplyr::select(ID:DOY, dist_to_VF, VF_EX, geometry)
+str(GPS_all)
 
 
+GPS_all <- GPS_all %>% dplyr::select(ID:DOY, dist_to_VF, VF_EX, geometry)
 
-coordinates <-as.data.frame( st_coordinates(GPS_sheep2))
-GPS_sheep2_df <- as.data.frame(GPS_sheep2)
 
-GPS_sheep2_df <- GPS_sheep2_df %>% 
+
+coordinates <-as.data.frame( st_coordinates(GPS_all))
+GPS_all_df <- as.data.frame(GPS_all)
+
+GPS_all_df <- GPS_all_df %>% 
   dplyr::select(-"geometry")
 
-names(GPS_sheep2_df)
-GPS_sheep2_df <-   cbind(GPS_sheep2_df,coordinates )
+names(GPS_all_df)
+GPS_all_df <-   cbind(GPS_all_df,coordinates )
 
 
 
@@ -112,8 +113,8 @@ GPS_sheep2_df <-   cbind(GPS_sheep2_df,coordinates )
 
 path_output_files <- "W:/VF/Optimising_VF/Waikerie/data_prep/" 
 path_output_files
-write.csv(GPS_sheep2_df, 
-          paste0(path_output_files,"/GPS_sheep2_df_dist_line_VF_zone_test.csv"), 
+write.csv(GPS_all_df, 
+          paste0(path_output_files,"/GPS_all_dist_line_VF_zone_test.csv"), 
           row.names=FALSE)
 
 
