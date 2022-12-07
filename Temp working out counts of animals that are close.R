0c621750819e07d71733ac172aab0afb0166a97e
Ouzman <ouz001@csiro.au>
2022-12-07 15:31:18 +1030

    trying to get there

--- Temp working out counts of animals that are close.R
+++ Temp working out counts of animals that are close.R
@@ -2,39 +2,73 @@ library(dplyr)
 library(tidyverse)
 library(readr)
 library(lubridate)
+library("readxl")
+
+
+#####################################################################################
+### number of other animals close to animal min dist is 1.5 - this can be changed ###
+#####################################################################################
+
+
+## we have 4 treatments 66,33,100,control
+Min_dist <-1.5 #the filter for the min distance between animals
+
+### ---- start with 66 ----####
+
+list_of_comparsions <- read_excel("W:/VF/Optimising_VF/Waikerie/data_prep/list of animal comparisons.xlsx", 
+                                  sheet = "66 percent per sheep comaprsion")
+##sheep 19 has no data so remove it
+list_of_comparsions <- list_of_comparsions %>%  filter(is.na(problem)) 
+list_of_comparsions$sheep <- as.character(list_of_comparsions$sheep)
 
 
 matrix_66percent <- read_csv("W:/VF/Optimising_VF/Waikerie/data_prep/step6_dist_between_animals_matrix_66.csv") 
-matrix_66percent
-rm(df)
-reg_time_step <- matrix_66percent %>% 
-  distinct(time_step)
-df <- reg_time_step %>%  arrange(time_step)
-matrix_66percent
-
-#for treatment = 66 % for sheep7
-sheep_list7 <- c(
-  #"time_step",
-  "dist10vs7",
-  "dist15vs7",
-  #"19vs7",
-  "dist21vs7",
-  "dist27vs7",
-  "dist28vs7",
-  "dist33vs7",
-  "dist36vs7")
 
 
-sheep7 <-   matrix_66percent %>%  dplyr::select(all_of(sheep_list7 ))
+reg_time_step <- matrix_66percent %>%   distinct(time_step) #creates a df with regular time step
+
+df <- data.frame(reg_time_step=POSIXct(),
+                 numb_sheep_close=integer(),
+                 sheep=character()) #df for the loop to write to
 
 
-sheep7
+### ---- for treatment = 66 % for sheep7 ----####
+#sheep <- "7"
+#sheep <- "10"
+list_of_sheep <- c("7","10")
 
 
 
-sheep7$num <- ncol(sheep7) - rowSums(is.na(sheep7) | sheep7 == "")
+### loop use a list of sheep as a variable to create a list and the list becomes the input for the loop
+#for (list_of_sheep in list_of_sheep){
+  
+sheep_list_df_x <- list_of_comparsions %>%  filter(sheep == list_of_sheep[[1]]) %>% select(comparsions)
+sheep_list_x <- as.list(sheep_list_df_x)
+sheep_list_comparisonx <- c(sheep_list_x$comparsions)
+  #sheep_list_comparisonx <- "dist10vs7"
+  
+sheep_x <-   matrix_66percent %>%  dplyr::select(all_of(sheep_list_comparisonx)) #remove the time step and only select clms that relate to the sheep
 
-sheep7_matrix <-   matrix_66percent %>%  dplyr::select(time_step)
+sheep_x[sheep_x > Min_dist] <- NA #replace values >1.5 with NA
 
-sheep7_matrix <- cbind(sheep7_matrix, sheep7$num)
-#remove the extra 1 its generated
+sheep_x <- sheep_x %>%  
+  dplyr::mutate(count = ncol(sheep_x) - rowSums(is.na(sheep_x) | sheep_x == ""))  %>% 
+  dplyr::select(count )
+
+sheep_matrix <-   matrix_66percent %>%  dplyr::select(time_step)
+sheep_matrix <- cbind(sheep_matrix, sheep_x)
+
+
+sheep_matrix <- sheep_matrix %>%
+  rename(!!paste0(paste0("numb_sheep_close")) := count) %>% 
+  mutate(sheep = list_of_sheep[[1]])
+
+df_temp = left_join(reg_time_step, sheep_matrix)
+
+df <- rbind(df, df_temp) 
+
+#}    
+
+
+
+
