6442691e57fb7f33faa3212a611505b04a459120
Ouzman <ouz001@csiro.au>
2022-12-08 12:25:53 +1030

    fix up comparsions

--- step 6 loop for distance between sheep.R
+++ step 6 loop for distance between sheep.R
@@ -17,180 +17,22 @@ df <- reg_time_step %>%  arrange(time_step)
 
 
 list_of_comparsions <- read_excel("W:/VF/Optimising_VF/Waikerie/data_prep/list of animal comparisons.xlsx", 
-                                  sheet = "dist_between_animals100")
-##sheep 19 has no data so remove it
+                                  sheet = "dist_between_animals")
+
 list_of_comparsions <- list_of_comparsions %>%  filter(is.na(replication)) 
+list_of_comparsions <- list_of_comparsions %>%  filter(is.na(problem))
 
-sheep_list <- as.list(list_of_comparsions)
-sheep_list <- c(sheep_list$comparison)
-
-
-
-#for treatment = 100 % 
-# sheep_list <- c("2vs3",
-#                "3vs2",
-#                "5vs2",
-#                "13vs2",
-#                "14vs2",
-#                "17vs2",
-#                "22vs2",
-#                "30vs2",
-#                "35vs2",
-#                "3vs2",
-#                "5vs2",
-#                "13vs2",
-#                "14vs2",
-#                "17vs2",
-#                "22vs2",
-#                "30vs2",
-#                "35vs2",
-#                "13vs5",
-#                "14vs5",
-#                "17vs5",
-#                "22vs5",
-#                "30vs5",
-#                "35vs5",
-#                "14vs13",
-#                "17vs13",
-#                "22vs13",
-#                "30vs13",
-#                "35vs13",
-#                "17vs14",
-#                "22vs14",
-#                "30vs14",
-#                "35vs14",
-#                "22vs17",
-#                "30vs17",
-#                "35vs17",
-#                "30vs22",
-#                "35vs22",
-#                "35vs30"
-# )
+list_of_comparsions_100 <- list_of_comparsions %>%  filter(treatment == "0.66") 
+sheep_list_x <- list_of_comparsions_100
 
 
-#for treatment = 33 % 
-sheep_list <- c("9vs1",
-"11vs1",
-"12vs1",
-"23vs1",
-"25vs1",
-"31vs1",
-"32vs1",
-"34vs1",
-"11vs9",
-"12vs9",
-"23vs9",
-"25vs9",
-"31vs9",
-"32vs9",
-"34vs9",
-"12vs11",
-"23vs11",
-"25vs11",
-"31vs11",
-"32vs11",
-"34vs11",
-"23vs12",
-"25vs12",
-"31vs12",
-"32vs12",
-"34vs12",
-"25vs23",
-"31vs23",
-"32vs23",
-"34vs23",
-"31vs25",
-"32vs25",
-"34vs25",
-"32vs31",
-"34vs31",
-"34vs32")
 
-rm(df)
-df <- reg_time_step %>%  arrange(time_step)
-################################################################################
-#for treatment = control % 
-sheep_list <- c(
-"6vs4",
-"8vs4",
-"16vs4",
-"18vs4",
-"20vs4",
-"24vs4",
-"26vs4",
-"29vs4",
-"8vs6",
-"16vs6",
-"18vs6",
-"20vs6",
-"24vs6",
-"26vs6",
-"29vs6",
-"16vs8",
-"18vs8",
-"20vs8",
-"24vs8",
-"26vs8",
-"29vs8",
-"18vs16",
-"20vs16",
-"24vs16",
-"26vs16",
-"29vs16",
-"20vs18",
-"24vs18",
-"26vs18",
-"29vs18",
-"24vs20",
-"26vs20",
-"29vs20",
-"26vs24",
-"29vs24",
-"29vs26")
+sheep_list_x <- as.list(sheep_list_x)
+sheep_list_x <- c(sheep_list_x$comparison)
 
-rm(df)
-df <- reg_time_step %>%  arrange(time_step)
+sheep_list <- sheep_list_x
 
-###############################################################################
-#for treatment = 66 % 
-sheep_list <- c(
-"10vs7",
-"15vs7",
-#"19vs7",
-"21vs7",
-"27vs7",
-"28vs7",
-"33vs7",
-"36vs7",
-"15vs10",
-#"19vs10",
-"21vs10",
-"27vs10",
-"28vs10",
-"33vs10",
-"36vs10",
-#"19vs15",
-"21vs15",
-"27vs15",
-"28vs15",
-"33vs15",
-"36vs15",
-# "21vs19",
-# "27vs19",
-# "28vs19",
-# "33vs19",
-# "36vs19",
-"27vs21",
-"28vs21",
-"33vs21",
-"36vs21",
-"28vs27",
-"33vs27",
-"36vs27",
-"33vs28",
-"36vs28",
-"36vs33")
-rm(df)
+
 df <- reg_time_step %>%  arrange(time_step)
 
 for (sheep_list in sheep_list){
@@ -242,7 +84,7 @@ for (sheep_list in sheep_list){
 #write.csv(df, "W:/VF/Optimising_VF/Waikerie/data_prep/step6_dist_between_animals_matrix_100percent.csv")
 #write.csv(df, "W:/VF/Optimising_VF/Waikerie/data_prep/step6_dist_between_animals_matrix_33percent.csv")
 #write.csv(df, "W:/VF/Optimising_VF/Waikerie/data_prep/step6_dist_between_animals_matrix_control.csv")
-write.csv(df, "W:/VF/Optimising_VF/Waikerie/data_prep/step6_dist_between_animals_matrix_66.csv")
+#write.csv(df, "W:/VF/Optimising_VF/Waikerie/data_prep/step6_dist_between_animals_matrix_66.csv")
 
 
 ### missing number 19 not sure why?
