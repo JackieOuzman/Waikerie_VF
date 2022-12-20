47df4b525e53d18358710564271605333ef2f2ed
Ouzman <ouz001@csiro.au>
2022-12-20 12:26:08 +1030

    RF start

---
+++ 01_RandomForest_model.R
@@ -0,0 +1,137 @@
+
+# David Gobbett
+# 3-Nov-2022
+# 
+# 04_RandomForest_downsampled.R
+# 
+# 
+# 
+# 
+# Code used here is based on Valavi et al (2022) supplementary material, to be cited
+# as 
+# Valavi, R., Guillera-Arroita, G, Lahoz-Monfort, J.J. & Elith, J. (2021) Predictive
+# performance of presence-only species distribution models: a benchmark study with
+# reproducible code. Ecological Monographs. 
+# 
+# RF background material:
+# https://builtin.com/data-science/random-forest-algorithm
+
+
+#install.packages("randomForest")
+install.packages("Hmisc")
+
+
+libs <- c("tidyverse", "dplyr",  "ggplot2", "randomForest", "Hmisc")
+
+
+install.libraries <- function(lib=NULL){
+  new <- lib[!(lib %in% installed.packages()[, "Package"])]
+  if (length(new)){   
+    install.packages(new, dependencies = TRUE)
+  }
+} 
+
+load.libraries <- function(lib=NULL){
+  sapply(libs, require, character.only = TRUE)
+}
+
+install.libraries(libs)
+load.libraries(libs)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+#ver <- "v1c"
+
+baseDir <- "W:/VF/Optimising_VF/Waikerie/data_prep/"
+outDir <- "W:/VF/Optimising_VF/Waikerie/data_prep/"
+
+numRandPts <- 50000
+
+#aggregFactors <- c(1:7)    # using raster input data aggregated over this range of values
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# load data
+
+RF_df <- read_csv("W:/VF/Optimising_VF/Waikerie/data_prep/step9_RF_df_input.csv")
+str(RF_df)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+unique(RF_df$compliance_score)
+
+RF_df <- RF_df %>%
+       mutate(compliance_score = replace(compliance_score, compliance_score %in% c("non_compliant"), 1),
+              compliance_score = replace(compliance_score, compliance_score %in% c("compliant"), 0)) %>% 
+       mutate(compliance_score = as.factor(compliance_score))
+
+
+### the model wont run could because of the NA values or because its so unbalanced?
+#replace all the NA with zero 
+
+RF_df[ is.na(RF_df) ] <- 0
+
+str(RF_df_corr_input)
+RF_df_corr_input <- RF_df %>% dplyr::select(-compliance_score,-sheep, -treatment , -herd_postion )
+
+
+
+res2<-rcorr(as.matrix(RF_df_corr_input))
+# ++++++++++++++++++++++++++++
+# flattenCorrMatrix
+# ++++++++++++++++++++++++++++
+# cormat : matrix of the correlation coefficients
+# pmat : matrix of the correlation p-values
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+
+correaltion <- flattenCorrMatrix(res2$r, res2$P)
+correaltion <- correaltion %>%  arrange(cor)
+
+
+
+RF_model <- randomForest(formula = compliance_score ~.,
+            data = RF_df,
+            ntree = 1500,
+            #sampsize = smpsize,
+            replace = TRUE)
+
+
+names(RF_df)
+
+RF_df_subset <- RF_df %>% dplyr::select(-sheep,-treatment ,
+                                 -mean_dist_frm_VF_outside_inclusion,
+                                 -max_dist_frm_VF_outside_inclusion,
+                                 -total_pulse_Day1,-total_pulse_Day2,
+                                 - total_audio_Day1,-total_audio_Day2,
+                                 -mean_dist_day1,-mean_dist_day2,
+                                -ratio_Day1 ,
+                                -ratio_Day2)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+RF_model
+plot(RF_model)
+importance(RF_model)
+
+whats_important_for_model <- as.data.frame(importance(RF_model))
+whats_important_for_model <- whats_important_for_model %>%  arrange(desc(MeanDecreaseGini))
+whats_important_for_model
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+RF_subsetmodel <- randomForest(formula = compliance_score ~.,
+                         data = RF_df_subset,
+                         ntree = 1500,
+                         #sampsize = smpsize,
+                         replace = TRUE)
+
+RF_subsetmodel
+plot(RF_subsetmodel)
+importance(RF_subsetmodel)
+
+whats_important_for_model_subset <- as.data.frame(importance(RF_subsetmodel))
+whats_important_for_model_subset <- whats_important_for_model_subset %>%  arrange(desc(MeanDecreaseGini))
+whats_important_for_model_subset
+
