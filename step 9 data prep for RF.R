34c682ccd3c90d79ff8705487d71d84087bdb667
JackieOuzman <jackie.ouzman@csiro.au>
2023-06-21 10:25:19 +0930

    probably a waste of time but data for RF and plots

--- step 9 data prep for RF.R
+++ step 9 data prep for RF.R
@@ -29,44 +29,24 @@ RF_df <- collared_animals %>% distinct(sheep, .keep_all = TRUE) %>%
 ################################################################################
 ### Note these trials were only 2 days long - another idea is to only keep 100% trial VF collared animals
 ### Add DOY clm
+
+date_13_14_keep_these <- c(2,3,5,13,14,17,22,30,35) #100% trial was run on the 13th and 14th 
+
+
+### Add DOY clm
 temp <- collared_animals %>% 
   filter(!is.na(DOY ))
 
 min_DOY <- min(temp$DOY, na.rm = TRUE)
 max_DOY <- max(temp$DOY, na.rm = TRUE)
 
-
-date_13_14_keep_these <- c(2,3,5,13,14,17,22,30,35) #100% trial was run on the 13th and 14th 
-date_15_16_keep_these <- c(12,23,25, 10,15,21,27,33,36)  # 33% and 66% trial was run 15th and 16th
-
-
-Check_1 <- temp %>%
-  filter(date == "2018-03-13" | date == "2018-03-14")  #%>%
-filter(sheep %in% date_13_14_keep_these)
-Check_1_min_DOY <- min(Check_1$DOY, na.rm = TRUE)
-
-Check_2 <-  temp %>%  
-  filter(date == "2018-03-15" |date == "2018-03-16")  %>%
-  filter(sheep %in% date_15_16_keep_these)
-Check_2_min_DOY <- min(Check_2$DOY, na.rm = TRUE)
-
-Check_1 <- Check_1 %>% 
-  mutate(DOT = (DOY - Check_1_min_DOY)+1 )
-
-Check_2 <- Check_2 %>% 
-  mutate(DOT = (DOY - Check_2_min_DOY)+1 )
-
-
-new_DOT <- rbind(Check_1, Check_2)
-new_DOT <- new_DOT %>%  dplyr::select(ID, DOT)
-
-#remove old day of trial 
-collared_animals <- collared_animals %>% dplyr::select(-DOT)
-collared_animals <- left_join(collared_animals, new_DOT)  
+collared_animals <- collared_animals %>% 
+  mutate(DOT = (DOY - min_DOY)+1 )
 
 collared_animals$DOY <- as.double(collared_animals$DOY )
 
-rm(Check_2, Check_1, new_DOT, temp)
+
+rm(temp)
 ################################################################################
 ### Definition of early behaviour ### Note these trials were only 2 days long
 ################################################################################
@@ -92,17 +72,17 @@ hours_behav <-  hours_behav %>% mutate(mins = n *10) #each data point is a 10min
 
 hours_behav
 
-##---- up to here -----##
+
 #####################################################################################
 ### add in the distance between animals
 ######################################################################################
 
-dist_bewteen <- read_csv("W:/VF/Optimising_VF/Waikerie/data_prep/step7_count_close_animals.csv")
+dist_bewteen <- read_csv("W:/VF/Optimising_VF/Waikerie/data_prep/step7_count_close_animals_100percent.csv")
 
 
 
 ## make a new variable hours
-dist_bewteen$time_step <- as.POSIXct(dist_bewteen$time_step,  tz = "Australia/Adelaide")
+dist_bewteen$time_step <- as.POSIXct(dist_bewteen$time_step,  tz = "Australia/Melbourne")
 dist_bewteen <- dist_bewteen %>%  dplyr::mutate(date= date(time_step))  
 
 #### add to the collared animals df
@@ -173,31 +153,16 @@ str(collared_animals)
 
 dist_taken_summary <- collared_animals %>%  group_by(sheep) %>% 
   summarise(total_dist_travel  =sum(step, na.rm=TRUE))
+
+dist_taken_summary <- left_join(dist_taken_summary, hours_behav)
+dist_taken_summary <- dist_taken_summary %>% 
+  mutate(dist_travel_ratio = total_dist_travel/ mins)
 dist_taken_summary
 
-RF_df <- left_join(RF_df, dist_taken_summary)
 
+RF_df <- left_join(RF_df, dist_taken_summary)
 rm(dist_taken_summary)
 
-
-mean_dist_taken_day1_2_summary <- collared_animals %>%  group_by(sheep, DOT) %>% 
-  summarise(mean_dist_travel  =mean(step, na.rm=TRUE))
-mean_dist_taken_day1_2_summary
-
-
-
-mean_dist_taken_day1_2_summary_wide <- mean_dist_taken_day1_2_summary %>% 
-  pivot_wider(names_from = DOT , 
-              values_from = mean_dist_travel)
-mean_dist_taken_day1_2_summary_wide
-mean_dist_taken_day1_2_summary_wide <- mean_dist_taken_day1_2_summary_wide %>% 
-  rename(mean_dist_day1 = `1`,
-         mean_dist_day2 = `2`)
-
-
-RF_df <- left_join(RF_df, mean_dist_taken_day1_2_summary_wide)
-
-rm(mean_dist_taken_day1_2_summary_wide)
 
 
 #####################################################################################
@@ -222,44 +187,16 @@ collared_animals <- collared_animals %>%
 
 
 cue_summary <- collared_animals %>%  group_by(sheep) %>% 
-  summarise(total_audio  =sum(audio, na.rm=TRUE),
-            total_pulse  =sum(pulse, na.rm=TRUE),
-            ratio = total_audio/(total_pulse+total_audio)*100)
+  summarise(mean_audio  =mean(audio, na.rm=TRUE),
+            mean_pulse  =mean(pulse, na.rm=TRUE),
+            mean_ratio = mean_audio/(mean_pulse+mean_audio)*100)
+
 cue_summary
 cue_summary[ is.na(cue_summary) ] <- NA
-
 
 RF_df <- left_join(RF_df, cue_summary)
 rm(cue_summary)
 
-
-cue_DOT_summary <- collared_animals %>%  group_by(sheep, DOT) %>% 
-  summarise(total_audio  =sum(audio, na.rm=TRUE),
-            total_pulse  =sum(pulse, na.rm=TRUE),
-            ratio = total_audio/(total_pulse+total_audio)*100)
-
-cue_DOT_summary_wide <- cue_DOT_summary %>% 
-  pivot_wider(names_from = DOT , 
-              values_from = c(total_audio, total_pulse, ratio))
-
-
-cue_DOT_summary_wide
-
-cue_DOT_summary_wide <- cue_DOT_summary_wide %>% 
-  rename(total_audio_Day1 = total_audio_1,
-         total_audio_Day2 = total_audio_2,
-         
-         total_pulse_Day1 = total_pulse_1,
-         total_pulse_Day2 = total_pulse_2,
-         
-         ratio_Day1 = ratio_1,
-         ratio_Day2 = ratio_2)
-         
-cue_DOT_summary_wide[ is.na(cue_DOT_summary_wide) ] <- NA  
-
-
-RF_df <- left_join(RF_df, cue_DOT_summary_wide)
-rm(cue_DOT_summary_wide)
 
 
 
@@ -269,95 +206,68 @@ rm(cue_DOT_summary_wide)
 ######################################################################################
 str(collared_animals)
 
-beha_summary <- collared_animals %>%  group_by(sheep) %>% 
-  summarise(total_resting   =sum(resting,  na.rm=TRUE),
-            total_standing  =sum(standing, na.rm=TRUE),
-            total_walking   =sum(walking,  na.rm=TRUE),  
-            total_running   =sum(running,  na.rm=TRUE),
-            total_beha      =  (total_resting+ total_standing+ total_walking+ total_running)                  
+###make new clms for resting grazing and moving so it matches the other dataset
+
+temp <- collared_animals %>% dplyr::select(ID, resting,standing , walking, running)
+
+temp <- temp %>% mutate(sum_activity = resting+ standing+walking+running)
+temp <- temp %>% mutate(grazing = standing)
+temp <- temp %>% mutate(moving = walking+ running)
+temp <- temp %>% dplyr::select(ID,resting,moving, grazing )
+
+collared_animals <- collared_animals %>% dplyr::select(-resting,-standing , -walking, -running)
+collared_animals <- left_join(collared_animals,temp)
+
+rm(temp)
+
+beha_summary_prop <- collared_animals %>%  
+  #group_by(sheep, behaviour_stage) %>%  
+  dplyr::mutate(
+    prop_resting = resting*100,
+    prop_grazing = grazing*100,
+    prop_moving = moving*100    )
+
+beha_summary <- beha_summary_prop %>%  group_by(sheep) %>% 
+  summarise(mean_resting   =mean(prop_resting,  na.rm=TRUE),
+            mean_grazing  =mean(prop_grazing , na.rm=TRUE),
+            mean_moving   =mean(prop_moving,  na.rm=TRUE)  
   )                  
 
-beha_summary <- beha_summary %>% 
-  dplyr::mutate(
-    prop_resting = (total_resting/total_beha)*100,
-    prop_standing = (total_standing/total_beha)*100,
-    prop_walking = (total_walking/total_beha)*100,
-    prop_running = (total_running/total_beha)*100,
-    check = (prop_resting+prop_standing+prop_walking+prop_running)
-    )
-beha_summary <-beha_summary %>%  dplyr::select(sheep, prop_resting,prop_standing,prop_walking,prop_running)       
+beha_summary
 
 RF_df <- left_join(RF_df, beha_summary)
-rm(beha_summary)
+rm(beha_summary, beha_summary_prop)
+
 
 
 #####################################################################################
 ### add in the distance between animals
 ######################################################################################
+dist_bewteen <- collared_animals %>% 
+  group_by(sheep) %>% 
+  summarise(mean_numb_sheep_close    =mean(numb_sheep_close ,  na.rm=TRUE))
 
-dist_bewteen_animals_33 <- read_csv("W:/VF/Optimising_VF/Waikerie/data_prep/step7_count_close_animals_33percent.csv")
-dist_bewteen_animals_66 <- read_csv("W:/VF/Optimising_VF/Waikerie/data_prep/step7_count_close_animals_66percent.csv")
-dist_bewteen_animals_100 <- read_csv("W:/VF/Optimising_VF/Waikerie/data_prep/step7_count_close_animals_100percent.csv")
+dist_bewteen
 
 
-## make a new variable hours
-dist_bewteen_animals_33$time_step <- as.POSIXct(dist_bewteen_animals_33$time_step,  tz = "Australia/Adelaide")
-dist_bewteen_animals_33 <- dist_bewteen_animals_33 %>%  dplyr::mutate(date= date(time_step))  
+RF_df <- left_join(RF_df, dist_bewteen)
+rm(dist_bewteen)
+######################################################################################
 
-dist_bewteen_animals_66$time_step <- as.POSIXct(dist_bewteen_animals_66$time_step,  tz = "Australia/Adelaide")
-dist_bewteen_animals_66 <- dist_bewteen_animals_66 %>%  dplyr::mutate(date= date(time_step))  
-
-dist_bewteen_animals_100$time_step <- as.POSIXct(dist_bewteen_animals_100$time_step,  tz = "Australia/Adelaide")
-dist_bewteen_animals_100 <- dist_bewteen_animals_100 %>%  dplyr::mutate(date= date(time_step))  
 
 #keep sheep with collars only on the days that the trials were run for them
- 
-date_13_14_keep_these <- c(2,3,5,13,14,17,22,30,35) #100% trial was run on the 13th and 14th
-date_15_16_keep_these <- c(12,23,25, 10,15,21,27,33,36)  # 33% and 66% trial was run 15th and 16th
-
-
-dist_bewteen_animals_100 <- dist_bewteen_animals_100 %>%
-  filter(date == "2018-03-13" | date == "2018-03-13")  %>%
-  filter(sheep %in% date_13_14_keep_these)
-
-
-dist_bewteen_animals_66 <-
-  dist_bewteen_animals_66 %>%  
-  filter(date == "2018-03-15" |date == "2018-03-16")  %>%
-  filter(sheep %in% date_15_16_keep_these)
-
-dist_bewteen_animals_33 <-
-  dist_bewteen_animals_33 %>%
-  filter(date == "2018-03-15" | date == "2018-03-16")  %>%
-  filter(sheep %in% date_15_16_keep_these)
-
-
-dist_bewteen_animals_all <- rbind(dist_bewteen_animals_33, dist_bewteen_animals_66, dist_bewteen_animals_100)
-rm(dist_bewteen_animals_33,dist_bewteen_animals_66,  dist_bewteen_animals_100)
-
-
-
-
-
 ## we have a problem the data has a heap of zero some of these relate to when the trial was run and not run
-#for example on the 13/3 and 14/3 
-#This step check df if I have got al of these -  i have no zero in check file - so its all good.
-
-check_what_to_remove <- dist_bewteen_animals_all %>%  group_by(date,sheep) %>% 
-  summarise(mean_number_animals_close = mean(numb_sheep_close, na.rm=TRUE))  %>% 
-  arrange(sheep, date)
 
 
+str(RF_df)
 
-mean_number_close_animals<- dist_bewteen_animals_all %>%  group_by(sheep) %>% 
-  summarise(mean_number_animals_close = mean(numb_sheep_close, na.rm=TRUE))  %>% 
-  arrange(sheep)
-
-mean_number_close_animals
+## drop a few 
+RF_df <- RF_df %>% 
+  dplyr::select (-max_dist_frm_VF_inside_inclusion )
 
 
-RF_df <- left_join(RF_df, mean_number_close_animals)
-rm(mean_number_close_animals)
+
+
 
 
 
